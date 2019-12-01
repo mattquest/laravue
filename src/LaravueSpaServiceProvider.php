df9c0f2580d1cb61808e64f0b330ede54b2f4839
Matt Rademacher <mrademacher@gmail.com>
2019-12-01 09:57:40 -0800

    initial commit

--- src/LaravueSpaServiceProvider.php
+++ src/LaravueSpaServiceProvider.php
@@ -6,7 +6,7 @@ class LaravueSpaServiceProvider extends ServiceProvider {
   {
     $this->loadRoutesFrom(__DIR__.'/routes/web.php');
     $this->loadViewsFrom(__DIR__.'/resources/views', 'laravue-spa');
-    $this->publishes([__DIR__.'/resources/js' => resource_path('js/laravue-spa')], 'laravue-spa');
+    $this->publishes([__DIR__.'/resources/js/laravue' => resource_path('js')], 'laravue-spa');
     $this->publishes([__DIR__.'/config.php' => config_path('laravue-spa.php')], 'laravue-spa');
   }
   public function register()
