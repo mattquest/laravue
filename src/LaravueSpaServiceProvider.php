1808baf13cbac893ad4b09aa8e4be786ce3e1eb8
Matt Rademacher <mrademacher@gmail.com>
2019-12-04 21:39:06 -0800

    remove defunct config file

--- src/LaravueSpaServiceProvider.php
+++ src/LaravueSpaServiceProvider.php
@@ -7,7 +7,6 @@ class LaravueSpaServiceProvider extends ServiceProvider {
     $this->loadRoutesFrom(__DIR__.'/routes/web.php');
     $this->loadViewsFrom(__DIR__.'/resources/views', 'laravue-spa');
     $this->publishes([__DIR__.'/resources/js/laravue' => resource_path('js/laravue')], 'laravue-spa');
-    $this->publishes([__DIR__.'/config.php' => config_path('laravue-spa.php')], 'laravue-spa');
   }
   public function register()
   {
