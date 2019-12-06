7a020b036044e6207c2dd1f392de3c4b9db7f2d1
Matt Rademacher <mrademacher@gmail.com>
2019-12-05 18:51:31 -0800

    fix publish

--- src/LaravueSpaServiceProvider.php
+++ src/LaravueSpaServiceProvider.php
@@ -8,8 +8,10 @@ class LaravueSpaServiceProvider extends ServiceProvider {
     $this->loadViewsFrom(__DIR__.'/resources/views', 'laravue-spa');
     // publish all assets - for install
     // php artisan vendor:publish --tag="laravue-spa"
-    $this->publishes([__DIR__.'/resources/js/*' => resource_path('js/')], 'laravue-spa');
-    $this->publishes([__DIR__.'/Http/Controllers/*' => app_path('Http/Controllers/')], 'laravue-spa');
+    $this->publishes([
+      __DIR__.'/resources/js/' => resource_path('js'),
+      __DIR__.'/Http/Controllers/' => app_path('Http/Controllers')
+    ], 'laravue-spa');
     // publish core assets only - for update 
     // php artisan vendor:publish --tag="laravue-spa-core" --force
     $this->publishes([__DIR__.'/resources/js/laravue' => resource_path('js/')], 'laravue-spa-core');
